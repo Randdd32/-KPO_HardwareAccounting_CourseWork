e817fc35b553f01dcb3584bbac7b37f9f41e4c83
Илья <@WIN-RANNDDD>
2025-11-06 18:14:57 +0400

    Project initialization

---
+++ src/main/java/com/hardware/hardware_structure/HardwareStructureApplication.java
@@ -0,0 +1,13 @@
+package com.hardware.hardware_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareStructureApplication.class, args);
+	}
+
+}
